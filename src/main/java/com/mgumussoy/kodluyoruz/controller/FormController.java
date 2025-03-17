5a0992f04da90ee017b4fd704c6f0c566789a8ea
b21727283 <b21727283@cs.hacettepe.edu.tr>
2025-03-17 13:58:15 +0300

    validation, BindingResult

---
+++ src/main/java/com/mgumussoy/kodluyoruz/controller/FormController.java
@@ -0,0 +1,35 @@
+package com.mgumussoy.kodluyoruz.controller;
+
+import com.mgumussoy.kodluyoruz.dto.TeacherDto;
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@Log4j2
+public class FormController {
+
+    @GetMapping("/form")
+    public String getForm(Model model){
+        model.addAttribute("cv_teacher", new TeacherDto());
+        return "form_post/formvalidation";
+    }
+
+    @PostMapping("/form")
+    public String postForm(@Valid @ModelAttribute("cv_teacher") TeacherDto teacherDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            log.error("Error in form");
+            return "form_post/formvalidation";
+        }
+        log.info("Success!");
+        // db save
+        // file write
+        return "form_post/success";
+    }
+}
---
+++ src/main/java/com/mgumussoy/kodluyoruz/dto/TeacherDto.java
@@ -0,0 +1,30 @@
+package com.mgumussoy.kodluyoruz.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class TeacherDto {
+
+    private Long teacherId;
+
+    @NotEmpty(message = "Teacher name is required")
+    @Size(min=1, max=255)
+    private String teacherNameSurname;
+
+    @NotEmpty(message = "Teacher email is required")
+    @Email(message = "Please enter a valid email")
+    private String teacherEmail;
+
+    @NotEmpty(message = "Teacher password is required")
+    @Size(min=10, max=18, message = "Password must be between 10 and 18 characters")
+    private String teacherPassword;
+}
