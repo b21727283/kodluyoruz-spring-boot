12d8ae206c0120958d6efb2df8d93ba5296b66ed
b21727283 <b21727283@cs.hacettepe.edu.tr>
2025-03-19 16:51:39 +0300

    add authenticated user as the auditor

--- src/main/java/com/mgumussoy/kodluyoruz/config/AuditorAwareConfig.java
+++ src/main/java/com/mgumussoy/kodluyoruz/config/AuditorAwareConfig.java
@@ -3,6 +3,9 @@ package com.mgumussoy.kodluyoruz.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -11,6 +14,17 @@ public class AuditorAwareConfig {
 
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of("system");
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                return Optional.of(((UserDetails) principal).getUsername());
+            } else {
+                return Optional.of(principal.toString());
+            }
+        };
     }
 }
