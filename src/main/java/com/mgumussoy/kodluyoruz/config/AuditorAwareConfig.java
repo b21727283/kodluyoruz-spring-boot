afc26560126f6f47c1ff28387156c84170dea97b
b21727283 <b21727283@cs.hacettepe.edu.tr>
2025-03-19 16:59:33 +0300

    auditorProvider

--- src/main/java/com/mgumussoy/kodluyoruz/config/AuditorAwareConfig.java
+++ src/main/java/com/mgumussoy/kodluyoruz/config/AuditorAwareConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -19,12 +18,7 @@ public class AuditorAwareConfig {
             if (authentication == null || !authentication.isAuthenticated()) {
                 return Optional.empty();
             }
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetails) {
-                return Optional.of(((UserDetails) principal).getUsername());
-            } else {
-                return Optional.of(principal.toString());
-            }
+            return Optional.ofNullable(authentication.getName());
         };
     }
 }
